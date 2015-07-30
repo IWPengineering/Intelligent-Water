df6ac3fd494c4e321caf3144a161b1300f6092a1
Jacqui <jy1189@messiah.edu>
2015-07-30 15:37:46 -0500

    Creation of Intentional Conflict
    This is just practice for the GitHub guide.

---
+++ troubleshooting/Intentional Conflict.X/build/default/production/intentional Conflict.o.d
@@ -0,0 +1,1 @@
+build/default/production/intentional\ Conflict.o: intentional\ Conflict.c
---
+++ troubleshooting/Intentional Conflict.X/intentional Conflict.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   intentional Conflict.c
+ * Author: Jacqui
+ *
+ * Created on July 30, 2015, 3:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+// This is the Original Comment -Created on July 30th 2015 at 3:32 by Jacqui Young
+    return (EXIT_SUCCESS);
+}
+
